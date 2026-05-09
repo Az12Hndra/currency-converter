613898e9d36a42c66c3627fabec3b469bf67ddef
Az12Hndra <h.azizhendrawan@gmail.com>
2026-05-09 13:09:32 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+import requests
+
+def get_rates(from_currency):
+    pass
+
+def get_user_input():
+    pass
+
+def convert(rates, to_currency, amount):
+    pass
+
+def display_result():
+    pass
