13c36a9e720177c6753fb67a52ea1196567c7769
Az12Hndra <h.azizhendrawan@gmail.com>
2026-05-09 14:46:52 +0700

    Add get_rates function to retrieve currency rates
    Implemented get_rates function to fetch exchange rates from an API.

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import requests
+import json
 
 def get_rates(from_currency):
-    pass
+    api_key = '71ae59f702af30bf3184d6c4'
+    url = f'https://v6.exchangerate-api.com/v6/{api_key}/latest/{from_currency}'
+    response = requests.get(url)
+    data = response.json()
 
+    # print(json.dumps(data, indent=4))
+
+    if data['result'] != 'success':
+        print('Failed to fetch exchange rates. Check your internet connection and API key.')
+        return None
+    
+    return data['conversion_rates']
+    
 def get_user_input():
     pass
 
